bd1b0e441a8d2818b240691d7e31336ab083d6b1
Jackson Oppenheim <jacksonopp@gmail.com>
2022-06-19 15:18:35 -0500

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {key: 'Access-Control-Allow-Origin', value: 'localhost:3000'},
+    ]
+  }
 }
 
 module.exports = nextConfig
