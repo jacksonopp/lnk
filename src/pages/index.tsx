f6c922582419e9e574b0bcda9aa40ac0c40339dd
Jackson Oppenheim <jacksonopp@gmail.com>
2022-06-19 11:37:29 -0500

    added basic form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [url, setUrl] = useState("");
+
   return (
     <div>
       <Head>
@@ -10,13 +13,42 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="text-3xl font-bold text-blue-500">
-          Hello World
-        </h1>
+      <main className="flex flex-col items-center justify-center h-screen">
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("submit", url);
+          }}
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="url"
+            >
+              Paste in your URL
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="url"
+              type="text"
+              placeholder="url"
+              value={url}
+              onChange={(e) => setUrl(e.currentTarget.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
