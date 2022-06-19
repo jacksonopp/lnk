0a1290618cd8add8bd3d68e859092394ca59c514
Jackson Oppenheim <jacksonopp@gmail.com>
2022-06-19 16:03:59 -0500

    temp change localhost

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default withTRPC<AppRouter>({
   config({ctx}) {
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+      : 'http://localhost:3001/api/trpc';
 
       return {
         url
