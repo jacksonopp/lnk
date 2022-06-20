7e731c639f465913efc833ea9dc73c985aff07c2
Jackson Oppenheim <jacksonopp@gmail.com>
2022-06-19 22:34:29 -0500

    temp cron job and non-destructive endpoint

--- src/pages/api/cron/index.ts
+++ src/pages/api/cron/index.ts
@@ -4,16 +4,42 @@ import { formatISO } from "date-fns"
 
 // a next.js handler function that removes all expired shortlinks
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const data = await prisma.shortLink.deleteMany({
-    where: {
-      expiresAt: {
-        lte: formatISO(new Date())
-      }
-    }
-  })
+  if (req.method === 'POST') {
+    try {
+      const { authorization } = req.headers;
 
-  return res.status(200).json({
-    message: "All expired shortlinks have been deleted",
-    data
-  })
+      console.log('cleaning up expired shortlinks...');
+
+      if (authorization !== `Bearer ${process.env.API_SECRET_KEY}`) {
+        res.status(401).json({
+          error: 'Unauthorized',
+          success: false
+        })
+      } else {        
+        const data = await prisma.shortLink.findMany({
+          where: {
+            expiresAt: {
+              lte: formatISO(new Date())
+            }
+          }
+        })
+
+        console.log('shortlinks have been cleaned up');
+      
+        return res.status(200).json({
+          message: "All expired shortlinks have been deleted",
+          data
+        })
+      }
+
+    } catch (error: any) {
+      res.status(500).json({statusCode: 500, message: error.message})
+    }
+  
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
+
+
 }
