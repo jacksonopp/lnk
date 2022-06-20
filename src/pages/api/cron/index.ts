dc661a9264ef271660075e6a100efeecfd22fd7c
Jackson Oppenheim <jacksonopp@gmail.com>
2022-06-19 21:56:41 -0500

    fix comparison type

--- src/pages/api/cron/index.ts
+++ src/pages/api/cron/index.ts
@@ -1,17 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import prisma from "../../../../prisma/prisma"
+import { formatISO } from "date-fns"
 
 // a next.js handler function that removes all expired shortlinks
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await prisma.shortLink.deleteMany({
+  const data = await prisma.shortLink.deleteMany({
     where: {
       expiresAt: {
-        lte: new Date()
+        lte: formatISO(new Date())
       }
     }
   })
 
   return res.status(200).json({
-    message: "All expired shortlinks have been deleted"
+    message: "All expired shortlinks have been deleted",
+    data
   })
 }
