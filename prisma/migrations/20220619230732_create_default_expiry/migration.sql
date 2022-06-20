7e3b52b60cba02f94c3a2c291c993546c7a66001
Jackson Oppenheim <jacksonopp@gmail.com>
2022-06-19 20:28:54 -0500

    completely reset migration

--- prisma/migrations/20220619230732_create_default_expiry/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `expiresIn` on table `ShortLink` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "ShortLink" ALTER COLUMN "expiresIn" SET NOT NULL,
-ALTER COLUMN "expiresIn" SET DEFAULT 3600;
--- prisma/migrations/20220620012841_init/migration.sql
+++ prisma/migrations/20220620012841_init/migration.sql
@@ -1,6 +1,8 @@
 -- CreateTable
 CREATE TABLE "ShortLink" (
     "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresIn" INTEGER NOT NULL DEFAULT 3600,
     "url" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
 
